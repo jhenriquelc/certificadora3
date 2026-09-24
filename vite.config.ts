60c2bc2483232d12562e7cda9d79977a31fbaa49
João H. L. Corrêa <jhenriquelc@gmail.com>
2026-09-24 10:21:25 -0300

    chore(vite): update deprecated `__dirname` usage

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 
 const dirname =
-	typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+	typeof import.meta.dirname !== 'undefined' ? import.meta.dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
